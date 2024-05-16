e5c2b6baf30f0c418744d38727d2296e39025809
jhreyes483 <jhreyes483@misena.edu.co>
2024-05-16 18:22:41 -0500

    creacion de componente

---
+++ project-angular/src/app/components/micomponente/micomponente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-micomponente',
+  templateUrl: './micomponente.component.html',
+  styleUrl: './micomponente.component.css'
+})
+export class MicomponenteComponent {
+
+}
